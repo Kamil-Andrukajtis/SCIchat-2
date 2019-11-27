5555d1ebba6c71dbd67e71c85c351e04f5746b2e
Kamil-Andrukajtis <44843860+Kamil-Andrukajtis@users.noreply.github.com>
2019-11-27 15:20:09 +0100

    1.12
    fixed username not showing up

--- scichat_sender.bat
+++ scichat_sender.bat
@@ -7,6 +7,6 @@ echo Wiadomosci przekraczajace 120 znakow nie zostana przyjete
 title SCIchat2 Sender - %Username%
 :pismo
 set /p Wiadomosc=%Username%:
-set Wiadomosc=<%Username%>:%Wiadomosc%
+set Wiadomosc=%Username%:%Wiadomosc%
 echo %Wiadomosc%> P:\SCI_CHAT\INBOX\%Username%.png
 goto pismo
