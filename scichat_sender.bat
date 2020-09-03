bb8fa48aedeacda597c82dcd6cc55339c01667a4
sciAndrew <44843860+sciAndrew@users.noreply.github.com>
2020-09-03 13:09:08 +0200

    1.14
    user name is forced to be the same as windows username

--- scichat_sender.bat
+++ scichat_sender.bat
@@ -1,7 +1,7 @@
 @echo off
 title SCIchat2 Sender
-set /p Username=Nazwa uzytkownika: 
-set Username=%RANDOM%%Username%
+set random=%RANDOM%
+set Username=%USERNAME%%RANDOM:~-3%
 echo twoja nazwa to %Username%
 echo Wiadomosci przekraczajace 120 znakow nie zostana przyjete
 title SCIchat2 Sender - %Username%
