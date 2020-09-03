0057da0894cc481f724fc9a2ecb6c2e7f23fdb56
sciAndrew <44843860+sciAndrew@users.noreply.github.com>
2020-09-03 12:53:58 +0200

    1.13
    removed automatic maintainer installation

--- start_scichat.bat
+++ start_scichat.bat
@@ -1,5 +1,3 @@
-if exist "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\scichat_maintainer.bat" (start uninstall_scichat_maintainer.bat)
-if not exist "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\scichat_maintainer.bat" ( copy "scichat_maintainer.bat" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" )
 start /I scichat_maintainer.bat
 timeout /nobreak 1
 start /I scichat_reader.bat
